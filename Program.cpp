3c00cb399e25eeb58c39e23c625864b123fffa4d
DckFgtsn <ebanushka2005@gmail.com>
2024-05-19 13:27:16 +0300

    start

---
+++ Program.cpp
@@ -0,0 +1,14 @@
+// ===========================================================================
+// Program.cpp // Bridge Pattern
+// ===========================================================================
+
+void test_conceptual_example_01();
+void test_conceptual_example_02();
+void test_shapes_example();
+
+int main(){
+    test_conceptual_example_01();
+    test_conceptual_example_02();
+    test_shapes_example();
+    return 0;
+}
